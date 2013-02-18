b36306ba43fc0dc81caa63827c8c201a999d49e6
Jerrico Gamis <jecklgamis@gmail.com>
2013-02-18 20:01:09 +1100

    Deleted unused files, fixed Makefile.am for installed headers

--- tests/rcunit_test_analysis.c
+++
@@ -1,7 +0,0 @@
-DEF_LOCAL_TEST_FUNC(rcu_test_analysis_001){
-    RCU_LOG_INFO_P1("sizeof(RCU_TEST_FUNCTION) = %d",sizeof(RCU_TEST_FUNCTION));
-    RCU_LOG_INFO_P1("sizeof(RCU_TEST_MODULE) = %d",sizeof(RCU_TEST_MODULE));
-    RCU_LOG_INFO_P1("sizeof(RCU_TEST_REGISTRY) = %d",sizeof(RCU_TEST_REGISTRY));
-    RCU_LOG_INFO_P1("sizeof(RCU_TEST_MACHINE) = %d",sizeof(RCU_TEST_MACHINE));
-    return(RCU_E_OK);
-}
--- tests/rcunit_test_random_emelgeek.c
+++
@@ -1,5 +0,0 @@
-
-DEF_LOCAL_TEST_FUNC(rcu_test_random_emelgeek_001){
-    return(RCU_E_OK);
-}
-
