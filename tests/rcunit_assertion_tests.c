05aaeecf90b3291dbfd40919f3f606d27396086f
Jerrico Gamis <jecklgamis@gmail.com>
2026-05-09 17:51:27 +1000

    Tests: expand assertion test coverage
    Add tests for RCU_ASSERT_NOT_EQUAL, GT/LT/GE/LE/IN_RANGE, NEAR,
    NOT_EQUAL_PTR, BIT_NOT_SET, BITS_SET/NOT_SET, ZERO_MEM, EQUAL_ARRAY,
    and string macros (NOT_EQUAL_STR, STR_CONTAINS, STR_EMPTY, STR_NOT_EMPTY).
    Fix duplicate license URL line.

--- tests/rcunit_assertion_tests.c
+++ tests/rcunit_assertion_tests.c
@@ -6,7 +6,6 @@
  * You may obtain a copy of the License at
  *
  *     https://www.apache.org/licenses/LICENSE-2.0
- *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +14,7 @@
  * limitations under the License.
  */
 
-#include "rcunit_test.h" 
+#include "rcunit_test.h"
 
 static int nr_invocations = 0;
 
@@ -34,16 +33,16 @@ const void *return_null_ptr() {
     return NULL;
 }
 
-/* Verify that assertion statements evaluate the condition only once 
- */
+/* Verify that assertion statements evaluate the condition only once */
 TMK_TEST(rcu_test_assert_single_evaluation) {
     nr_invocations = 0;
     RCU_ASSERT(return_1());
     TMK_ASSERT_EQUAL(1, nr_invocations);
+
     nr_invocations = 0;
     RCU_ASSERT_TRUE(return_1());
     TMK_ASSERT_EQUAL(1, nr_invocations);
-    
+
     nr_invocations = 0;
     RCU_ASSERT_FALSE(!return_1());
     TMK_ASSERT_EQUAL(1, nr_invocations);
@@ -59,17 +58,18 @@ TMK_TEST(rcu_test_assert_single_evaluation) {
     nr_invocations = 0;
     RCU_ASSERT_NULL(return_null_ptr());
     TMK_ASSERT_EQUAL(1, nr_invocations);
-    
+
     nr_invocations = 0;
     RCU_ASSERT_NOT_NULL(return_string());
     TMK_ASSERT_EQUAL(1, nr_invocations);
 }
 
-RCU_TEST(rcu_bit_assertion_tests){
+RCU_TEST(rcu_bit_assertion_tests) {
     rcu_u2 data;
-    
+
     data = 1;
     RCU_ASSERT_BIT_SET(data, 0);
+    RCU_ASSERT_BIT_NOT_SET(data, 1);
 
     data = 128;
     RCU_ASSERT_BIT_SET(data, 7);
@@ -79,12 +79,15 @@ RCU_TEST(rcu_bit_assertion_tests){
 
     data = 32768;
     RCU_ASSERT_BIT_SET(data, 15);
-    
+
     data = 32768 + 256 + 128 + 1;
     RCU_ASSERT_BIT_SET(data, 0);
     RCU_ASSERT_BIT_SET(data, 7);
     RCU_ASSERT_BIT_SET(data, 8);
     RCU_ASSERT_BIT_SET(data, 15);
+
+    RCU_ASSERT_BITS_SET(data, 0x8181);
+    RCU_ASSERT_BITS_NOT_SET(data, 0x0002);
 }
 
 TMK_TEST(rcu_test_bit_assertions) {
@@ -92,20 +95,38 @@ TMK_TEST(rcu_test_bit_assertions) {
     TMK_ASSERT_EQUAL(RCU_E_OK, rcu_run_tests());
 }
 
-RCU_TEST(rcu_byte_assertion_tests){
+RCU_TEST(rcu_integer_assertion_tests) {
+    RCU_ASSERT_NOT_EQUAL(1, 2);
+    RCU_ASSERT_GT(5, 3);
+    RCU_ASSERT_LT(3, 5);
+    RCU_ASSERT_GE(5, 5);
+    RCU_ASSERT_GE(6, 5);
+    RCU_ASSERT_LE(5, 5);
+    RCU_ASSERT_LE(4, 5);
+    RCU_ASSERT_IN_RANGE(5, 1, 10);
+    RCU_ASSERT_IN_RANGE(1, 1, 10);
+    RCU_ASSERT_IN_RANGE(10, 1, 10);
+}
+
+TMK_TEST(rcu_test_integer_assertions) {
+    rcu_add_test(rcu_integer_assertion_tests);
+    TMK_ASSERT_EQUAL(RCU_E_OK, rcu_run_tests());
+}
+
+RCU_TEST(rcu_byte_assertion_tests) {
     rcu_u1 expected[4];
 
     expected[0] = 0xde;
     expected[1] = 0xad;
     expected[2] = 0xc0;
     expected[3] = 0xde;
-    
+
     rcu_u1 actual[4];
     actual[0] = 0xde;
     actual[1] = 0xad;
     actual[2] = 0xc0;
     actual[3] = 0xde;
-    
+
     RCU_ASSERT_EQUAL_MEM(expected, actual, 4);
 
     actual[0] = 0xde;
@@ -114,6 +135,13 @@ RCU_TEST(rcu_byte_assertion_tests){
     actual[3] = 0xef;
 
     RCU_ASSERT_NOT_EQUAL_MEM(expected, actual, 4);
+
+    rcu_u1 zeros[4] = {0, 0, 0, 0};
+    RCU_ASSERT_ZERO_MEM(zeros, 4);
+
+    long arr_expected[3] = {1, 2, 3};
+    long arr_actual[3]   = {1, 2, 3};
+    RCU_ASSERT_EQUAL_ARRAY(arr_expected, arr_actual, 3);
 }
 
 TMK_TEST(rcu_test_byte_assertions) {
@@ -124,6 +152,7 @@ TMK_TEST(rcu_test_byte_assertions) {
 RCU_TEST(rcu_float_assertion_tests) {
     RCU_ASSERT_EQUAL_FLOAT(1.00001, 1.00001);
     RCU_ASSERT_NOT_EQUAL_FLOAT(1.00001, 1.00000);
+    RCU_ASSERT_NEAR(1.0, 1.0001, 0.001);
 }
 
 TMK_TEST(rcu_test_float_assertions) {
@@ -133,8 +162,10 @@ TMK_TEST(rcu_test_float_assertions) {
 
 RCU_TEST(rcu_pointer_assertion_tests) {
     int x;
-    int *y = &x;
-    RCU_ASSERT_EQUAL_PTR(&x, y);
+    int y;
+    int *px = &x;
+    RCU_ASSERT_EQUAL_PTR(&x, px);
+    RCU_ASSERT_NOT_EQUAL_PTR(&x, &y);
 }
 
 TMK_TEST(rcu_test_pointer_assertions) {
@@ -142,6 +173,15 @@ TMK_TEST(rcu_test_pointer_assertions) {
     TMK_ASSERT_EQUAL(RCU_E_OK, rcu_run_tests());
 }
 
+RCU_TEST(rcu_string_assertion_tests) {
+    RCU_ASSERT_EQUAL_STR("hello", "hello");
+    RCU_ASSERT_NOT_EQUAL_STR("hello", "world");
+    RCU_ASSERT_STR_CONTAINS("hello world", "world");
+    RCU_ASSERT_STR_EMPTY("");
+    RCU_ASSERT_STR_NOT_EMPTY("hello");
+}
 
-
-
+TMK_TEST(rcu_test_string_assertions) {
+    rcu_add_test(rcu_string_assertion_tests);
+    TMK_ASSERT_EQUAL(RCU_E_OK, rcu_run_tests());
+}
